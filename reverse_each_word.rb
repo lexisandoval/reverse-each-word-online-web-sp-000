1b2ceed845f0f8847438a4b1531ee0e2bdf10ca1
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-14 17:42:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(words)
   new = words.split
   new_arr = []
   new.collect do |x|
-    y = y.reverse
-    new_arr << y
+    x = x.reverse
+    new_arr << x
   end
   new_arr.join(" ")
 end
